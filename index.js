b9a3e94b42320839da63f6abec5dab5d349f5284
Gadzimurad <gadzimuradg76@gmail.com>
2026-02-11 19:30:31 +0300

    04-homework: Исправлены прошлые замечания по JS

---
+++ index.js
@@ -0,0 +1,29 @@
+console.log("Hello, world!");
+
+const FirstCardrecolorButton = document.getElementById('recolor-the-first-card');
+const firstCatalogCard = document.querySelector('.product-card');
+const blueHashColor = '#0000FF';
+
+FirstCardrecolorButton.addEventListener('click', () => {
+  firstCatalogCard.style.backgroundColor = blueHashColor;
+});
+
+const greenHashColor = '#00FF00';
+
+const catalogCards = document.querySelectorAll('.product-card');
+const recolorAllCardsButton = document.getElementById('recolor-all-card-button');
+
+recolorAllCardsButton.addEventListener('click', () => {catalogCards.forEach((card) => card.style.backgroundColor = greenHashColor);});
+const googleOpenButton = document.getElementById('open-google-button');
+googleOpenButton.addEventListener('click', openGoogle);
+
+function openGoogle() {
+  const answer = confirm('Вы действительно хотите перейти на сайт гугл')
+  
+  if(answer) {
+    window.open('https://google.com');
+  } else {
+    console.log('Пользователь отменил переход на Google')
+  }
+
+}
