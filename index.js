f1635b9995fa9bc60e1b16592062f5affed7df27
Gadzimurad <gadzimuradg76@gmail.com>
2026-02-12 20:27:27 +0300

    (Homework №4): Fix previous JS errors

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 console.log("Hello, world!");
 
-const FirstCardrecolorButton = document.getElementById('recolor-the-first-card');
+const firstCardrecolorButton = document.getElementById('recolor-the-first-card');
 const firstCatalogCard = document.querySelector('.product-card');
 const blueHashColor = '#0000FF';
 
-FirstCardrecolorButton.addEventListener('click', () => {
+firstCardrecolorButton.addEventListener('click', () => {
   firstCatalogCard.style.backgroundColor = blueHashColor;
 });
 
@@ -13,7 +13,10 @@ const greenHashColor = '#00FF00';
 const catalogCards = document.querySelectorAll('.product-card');
 const recolorAllCardsButton = document.getElementById('recolor-all-card-button');
 
-recolorAllCardsButton.addEventListener('click', () => {catalogCards.forEach((card) => card.style.backgroundColor = greenHashColor);});
+recolorAllCardsButton.addEventListener('click', () => {
+  catalogCards.forEach((card) => card.style.backgroundColor = greenHashColor);
+});
+
 const googleOpenButton = document.getElementById('open-google-button');
 googleOpenButton.addEventListener('click', openGoogle);
 
@@ -27,3 +30,25 @@ function openGoogle() {
   }
 
 }
+
+const mainTitle = document.getElementById('main-title');
+
+if(mainTitle) {
+  mainTitle.addEventListener('mouseenter',() => {
+    console.log('Содержимое загаловка:', mainTitle.textContent);
+  });
+} else {
+  console.warn('Главный заголовок не найден')
+}
+
+const toggleButton = document.getElementById("toggle-btn");
+const titleText = document.getElementById*("title-text");
+
+toggleButton.addEventListener("click", () => {
+  toggleButton.classList.toggle("switch-color");
+});
+
+titleText.addEventListener('mouseover', () => {
+  console.log(titleText.textContent)
+  
+}) 
