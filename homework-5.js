d98ccc8c0cb8bd3fa2e6304554d58e660617377f
Gadzimurad <gadzimuradg76@gmail.com>
2026-02-21 12:43:11 +0300

    Homework-5: Fix syntax error

--- homework-5.js
+++ homework-5.js
@@ -1,38 +1,43 @@
 function showTemperature(city, temp) {
-  console.log(`Сейчас в ${city} , температура ${temp} градусов по Цельсию.`);
+    console.log(`Сейчас в ${city} температура ${temp} градусов по Цельсию.`);
 }
 
 showTemperature("Махачкале", 20);
 
-const LIGHT_SPEED = 299792458;
+const LIGHT_SPEED = 300000000;
 
-function lightspeedcomparison(speed) {
-  if (LIGHT_SPEED < speed) {
-    console.log('Сверх Световая скорость!');
-} else if (LIGHT_SPEED < speed) {
-  console.log('Субсветовая скорость!')
-} else {
-  console.log('Равна скорости света!')
-  }
+function lightspeedСomparison(speed) {
+    if (LIGHT_SPEED < speed) {
+        console.log("Сверхсветовая скорость!");
+    } else if (LIGHT_SPEED > speed) {
+        console.log("Субсветовая скорость!");
+    } else {
+        console.log("Равна скорости света!");
+    }
 }
+lightspeedСomparison(300000000);
 
-const communactionProduct =  ('Dell z9100-ON');
-const price = 456928;
+const communactionProduct = "Dell Z9100-ON";
+const PRICE = 456_928;
 
 function buyCommunicationProduct(budget) {
-  if (budget >= price) {
-    console.log(`Поздавляем вы преобрели ${communactionProduct}!`);
-  } else {
-    console.log(`К сожалению, у вас не достаточно средств вам не хватает ${price - budget} рублей для покупки ${communactionProduct}.`);
-  };
-} 
+    if (budget >= PRICE) {
+        console.log(`Поздравляем, вы преобрели ${communactionProduct}!`);
+    } else {
+        console.log(
+            `К сожалению, у вас не достаточно средств вам не хватает ${PRICE - budget} рублей для покупки ${communactionProduct}.`
+        );
+    }
+}
 buyCommunicationProduct(500000);
 
+const server = "HP ProLiant DL380 Gen10";
+const sanStorage = "HP MSA 2070 SAN Storage";
+const router = "Mikrotik CCR1036-8G-2S+EM";
 
-const server = 'HP ProLiant DL380 Gen10';
-const sanStorage = 'HP MSA 2070 SAN Storage';
-const route = 'Mikrotik CCR1036-8G-2S+EM';
-
-function systeme1c() {
-  console.log(`Для работы 1С:Предприятия 8.3 нам необходим сервер ${server}, SAN Storage ${sanStorage} и маршрутизатор ${route}.`);
-}; 
+function describe1cInfrastructure() {
+    console.log(
+        `Для работы 1С:Предприятия 8.3 нам необходим сервер ${server}, SAN Storage ${sanStorage} и маршрутизатор ${router}.`
+    );
+}
+describe1cInfrastructure();
