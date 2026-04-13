e109155c0a60bb2625330fd425037e33ec5134ee
Gadzimurad <gadzimuradg76@gmail.com>
2026-04-13 21:29:01 +0300

    (Homework №8): fix indentation

--- homework-8.js
+++ homework-8.js
@@ -2,14 +2,14 @@ import { products } from "./products.js";
 
 const productTemplate = document.querySelector('#product-card-template');
 function renderProductCards(products) {
-  products.forEach(product => {
+products.forEach(product => {
     const productClone = productTemplate.content.cloneNode(true);
     productClone.querySelector('.product-card__img').src = `images/${product.image}.png`;
     productClone.querySelector('.product-card__for-skin').textContent = product.skinType;
     productClone.querySelector('.product-card__title').textContent = product.title;
     productClone.querySelector('.product-card__description').textContent = product.description;
     const compositionList = productClone.querySelector('.product-card__composition');
-  product.composition.forEach(item => {
+product.composition.forEach(item => {
     const li = document.createElement('li');
     li.className = 'product-card__item';
     li.textContent = item;
@@ -17,7 +17,7 @@ function renderProductCards(products) {
 }); 
     productClone.querySelector('.product-card__price').innerHTML = ` ${product.price} &#8381;`;
     document.querySelector('.product-card-wrapper').appendChild(productClone);
-  });
+});
 }
 
 
