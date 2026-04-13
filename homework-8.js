7389cf7b6cb7e56f0a3d57c0432905a04570adce
Gadzimurad <gadzimuradg76@gmail.com>
2026-04-13 17:09:12 +0300

    (Homework №8): add composition rendering

--- homework-8.js
+++ homework-8.js
@@ -1,13 +1,20 @@
 import { products } from "./products.js";
 
 const productTemplate = document.querySelector('#product-card-template');
-function renderProductCards(dataArray) {
-  dataArray.forEach(product => {
+function renderProductCards(products) {
+  products.forEach(product => {
     const productClone = productTemplate.content.cloneNode(true);
     productClone.querySelector('.product-card__img').src = `images/${product.image}.png`;
     productClone.querySelector('.product-card__for-skin').textContent = product.skinType;
     productClone.querySelector('.product-card__title').textContent = product.title;
     productClone.querySelector('.product-card__description').textContent = product.description;
+    const compositionList = productClone.querySelector('.product-card__composition');
+  product.composition.forEach(item => {
+    const li = document.createElement('li');
+    li.className = 'product-card__item';
+    li.textContent = item;
+    compositionList.appendChild(li);
+}); 
     productClone.querySelector('.product-card__price').innerHTML = ` ${product.price} &#8381;`;
     document.querySelector('.product-card-wrapper').appendChild(productClone);
   });
